ff4acca5f4c19d67e6b0bcbc83fabd8fe5199b92
IndianAg0711 <joseph.villafranca@gmail.com>
2014-02-18 14:38:02 -0600

    html todo list

---
+++ public/todo-list.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>TODO List</head>
+<body>
+	<h1>TODO List</h1>
+		<ul>
+			<li>Go grocery shopping</li>
+			<li>Wash the car</li>
+			<li>Clean the garage</li>
+			<li>Plant tomatoes</li>
+		</ul>
+
+	<h1>Add a new item to the list?</hi>
+		<form method="POST" action="">
+			<p>
+				<label for="new_item">New Item:</label>
+				<input id="new_item" name="new_item" type="text">
+			</p>
+		</form>
+		
