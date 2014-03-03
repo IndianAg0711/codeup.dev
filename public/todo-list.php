144f6dc98be699eac9b084bbf7fd41b211ae10d2
IndianAg0711 <joseph.villafranca@gmail.com>
2014-03-03 13:59:39 -0600

    updated to throw exception if new item is of 0 or >240 length.

--- public/todo-list.php
+++ public/todo-list.php
@@ -7,7 +7,7 @@ require('filestore.php');
 
 $todo = new Filestore('list.txt');
 
-$list = $todo->read_lines();
+$list = $todo->read();
 
 if (count($_FILES) > 0 && $_FILES['uploaded_file']['error'] == 0) {
 	if ($_FILES['uploaded_file']['type'] == 'text/plain') {
@@ -18,18 +18,21 @@ if (count($_FILES) > 0 && $_FILES['uploaded_file']['error'] == 0) {
 		$saved_filename = $upload_dir . $uploaded_filename;
 		move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $saved_filename);
 
+		// Instantiating new object of class Filestore to read uploaded file
+		$upload = new Filestore($saved_filename);
+
 		// Open uploaded file
-		$imported_list = $todo->read_lines();
+		$imported_list = $upload->read();
 
 		// Overwrite existing list checkbox option
 		if (isset($_POST['overwrite']) && $_POST['overwrite'] == 1) {
 				$list = $imported_list;
-				$todo->write_lines($list);
+				$todo->write($list);
 		} else {
 
 				// Append list and Save
 				$list = array_merge($list, $imported_list);
-				$todo->write_lines($list);
+				$todo->write($list);
 		}
 
 	}	else {
@@ -38,12 +41,14 @@ if (count($_FILES) > 0 && $_FILES['uploaded_file']['error'] == 0) {
 }	
 
 // Adding a new item
-if ((isset($_POST['new_item'])) && ($_POST['new_item'] != '')) {
+if ((isset($_POST['new_item'])) && ($_POST['new_item'] != '') && ((strlen($_POST['new_item'])) <= 240)) {
 	array_push($list, $_POST['new_item']);
 
 	// Save file
-	$todo->write_lines($list);
+	$todo->write($list);
 
+} elseif (((isset($_POST['new_item'])) && ($_POST['new_item'] == '')) || (isset($_POST['new_item']) && ((strlen($_POST['new_item'])) > 240))) {
+	throw new Exception('New Item has to be between 1 and 240 characters.');
 }
 
 // Removing an item
@@ -52,7 +57,7 @@ if (isset($_GET['remove'])) {
    	unset($list[$item_to_remove]);
    
    	// Save file
-   	$todo->write_lines($list);
+   	$todo->write($list);
 	header("Location: todo-list.php");
 }
 ?>
