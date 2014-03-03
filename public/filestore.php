8c77f2cc5d15454b617b4c8334274b466077135a
IndianAg0711 <joseph.villafranca@gmail.com>
2014-03-03 14:00:09 -0600

    added condition to check for filetype and save accordingly

--- public/filestore.php
+++ public/filestore.php
@@ -4,15 +4,43 @@ class Filestore {
 
     public $filename = '';
 
-    function __construct($filename = '') 
+    private $is_csv = FALSE;
+
+    public function __construct($filename = '') 
     {
         $this->filename = $filename;
+        if (substr($filename, -3) == 'csv') {
+            $this->is_csv = TRUE;
+        }
+        else {
+            $this->is_csv = FALSE;
+        }
+    }
+
+    public function read()
+    {
+        if ($this->is_csv == TRUE) {
+            return $this->read_csv();
+        }
+        else {
+            return $this->read_lines();
+        }
+    }
+
+    public function write($array)
+    {
+        if ($this->is_csv == TRUE) {
+            return $this->write_csv($array);
+        }
+        else {
+            return $this->write_lines($array);
+        }
     }
 
     /**
      * Returns array of lines in $this->filename
      */
-    function read_lines()
+    private function read_lines()
     {
         $handle = fopen($this->filename, 'r');
         $string = fread($handle, filesize($this->filename));
@@ -23,7 +51,7 @@ class Filestore {
     /**
      * Writes each element in $array to a new line in $this->filename
      */
-    function write_lines($array)
+    private function write_lines($array)
     {
         $handle = fopen($this->filename, 'w');
         $item_string = implode("\n", $array);
@@ -34,7 +62,7 @@ class Filestore {
     /**
      * Reads contents of csv $this->filename, returns an array
      */
-    function read_csv()
+    private function read_csv()
     {
         $array = [];
         $handle = fopen($this->filename, 'r');
@@ -48,7 +76,7 @@ class Filestore {
     /**
      * Writes contents of $array to csv $this->filename
      */
-    function write_csv($array)
+    private function write_csv($array)
     {
         $handle = fopen($this->filename, 'w');
         foreach ($array as $fields) {
