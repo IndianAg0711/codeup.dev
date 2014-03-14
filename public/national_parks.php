b0826a4d11b4fe4fb7c5e5912bad15b8350b7189
IndianAg0711 <joseph.villafranca@gmail.com>
2014-03-14 14:02:12 -0500

    Added sort options and css

--- public/national_parks.php
+++ public/national_parks.php
@@ -13,6 +13,15 @@ if (isset($_GET['name'])) {
 }	elseif (isset($_GET['date_established'])) {
 	$database = $mysqli->query('SELECT * FROM national_parks ORDER BY date_established ASC');
 	// header("Location: national_parks.php");
+}	elseif (isset($_GET['name_d'])) {
+	$database = $mysqli->query('SELECT * FROM national_parks ORDER BY name DESC');
+	// header("Location: national_parks.php");
+}	elseif (isset($_GET['location_d'])) {
+	$database = $mysqli->query('SELECT * FROM national_parks ORDER BY location DESC');
+	// header("Location: national_parks.php");
+}	elseif (isset($_GET['date_established_d'])) {
+	$database = $mysqli->query('SELECT * FROM national_parks ORDER BY date_established DESC');
+	// header("Location: national_parks.php");
 }	else {
 	$database = $mysqli->query('SELECT * FROM national_parks');
 }
@@ -20,16 +29,19 @@ if (isset($_GET['name'])) {
 ?>
 <html>
 <head>
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 	<title></title>
 </head>
 <body>
-<h1>National Parks</h1>
-<p>Change order by: &nbsp;
-<a href='?name'>Name</a>&nbsp;<a href='?location'>Location</a>&nbsp;<a href='?date_established'>Date Established</a>
-</p>
-<table>
+<h1 class="text-center">National Parks</h1>
+<table class='table table-striped table-bordered'>
 	<tr>
-		<th></th><th>Name:</th><th>Location:</th><th>Description:</th><th>Date Established</th><th>Area in Acres</th>
+		<th></th>
+		<th>Name: <br><a href='?name'><span class="glyphicon glyphicon-sort-by-alphabet"></span></a>&nbsp;<a href='?name_d'><span class="glyphicon glyphicon-sort-by-alphabet-alt"></span></a></th>
+		<th>Location: <br><a href='?location'><span class="glyphicon glyphicon-sort-by-alphabet"></span></a>&nbsp;<a href='?location_d'><span class="glyphicon glyphicon-sort-by-alphabet-alt"></span></a></th>
+		<th>Description:</th>
+		<th>Date Established: <br><a href='?date_established'><span class="glyphicon glyphicon-sort-by-alphabet"></span></a>&nbsp;<a href='?date_established_d'><span class="glyphicon glyphicon-sort-by-alphabet-alt"></span></a></th>
+		<th>Area (acres):</th>
 	</tr>
 	<? while ($row = $database->fetch_assoc()) : ?>
 	<tr>
